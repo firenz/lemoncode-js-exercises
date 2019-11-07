b1c01d89736f5438f70f404cdcb62b0a7e76fca0
Alicia G <alicia.guardenoalbertos@gmail.com>
2019-11-07 18:42:16 +0100

    Coded exercise 04 about reading object properties in arrays

---
+++ src/js-exercises/exercise_04/read-book.ts
@@ -0,0 +1,27 @@
+// --- EJERCICIO 04 -- READ BOOKS  ------------------------------
+
+// Crea una función isBookRead que reciba una lista de libros y un título 
+// y devuelva si se ha leído o no dicho libro.
+// Un libro es un objeto con title como string y isRead como booleano. 
+// En caso de no existir el libro devolver false.
+// TIP: Existe un método de Array.prototype que te ayudará a 
+// buscar según un patrón.
+
+// -- OPCIONAL --
+// Utiliza Typescript para añadir los tipos adecuados.
+
+interface BookSettings{
+  title: string;
+  isRead: boolean;
+};
+
+function isBookReadTS(books: BookSettings[] = [], titleToSearch: string = ""): boolean {
+  // Implementation here
+  let book: BookSettings = books.find(x => x.title === titleToSearch);
+  return (book === null) ? false : book.isRead;
+}
+
+console.log(`## Exercise 04 - READ BOOKS -- TS Version ## 
+"Devastación" is read?: ${isBookReadTS(books, "Devastación")}
+"Canción de hielo y fuego" is read?: ${isBookReadTS(books,"Canción de hielo y fuego")}
+"Los Pilares de la Tierra" is read?: ${isBookReadTS(books, "Los Pilares de la Tierra")}`);
